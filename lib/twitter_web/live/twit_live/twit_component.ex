bd1275f1644bdf2610d4238836ed6d4a6417fc24
saumya <ksaumyasingh@gmail.com>
2022-07-26 23:16:35 +0530

    edit delete working fine but likes_count have one problem

--- lib/twitter_web/live/twit_live/twit_component.ex
+++ lib/twitter_web/live/twit_live/twit_component.ex
@@ -1,8 +1,8 @@
 defmodule TwitterWeb.TwitLive.TwitComponent do
   use TwitterWeb, :live_component
   def render(assigns) do
-   ~L"""
-   <div id="twit-<%= @post.id %>" class="twit">
+   ~H"""
+   <div id="twit-{ @twit.id }" class="twit">
     <div class="row">
       <div class="column column-10">
         <div class="twit-avatar"></div>
@@ -15,21 +15,35 @@ defmodule TwitterWeb.TwitLive.TwitComponent do
     </div>
     <div class="row">
       <div class="column">
+      <a href="#" phx-click="like" phx-target="{ @myself }">
         <i class='far fa-heart' style='color:red'></i> <%= @twit.likes_count %>
+      </a>
       </div>
       <div class="column">
+      <a href="#" phx-click="repost" phx-target="{ @myself }">
         <i class='fas fa-retweet' style='color:blue'></i> <%= @twit.reposts_count %>
+      </a>
       </div>
       <div class="column">
-        <%= live_patch to: Routes.twit_index_path(@socket, :edit, @post.id) do %>
-          <i class="material-icons" id="del" style="color:red"></i>
+        <%= live_patch to: Routes.twit_index_path(@socket, :edit, @twit.id) do %>
+          <i class="fa fa-edit" style="color:black"></i>
         <% end %>
-        <%= link to: "#", phx_click: "delete", phx_value_id: @post.id, data: [confirm: "Are you sure?"] do %>
-        <i class="material-icons" id="del" style="color:red"></i>
+        <%= link to: "#", phx_click: "delete", phx_value_id: @twit.id, data: [confirm: "Are you sure?"] do %>
+          <i class="material-icons" style="font-size:16px;color:red">delete</i>
         <% end %>
-      </div>
+    </div>
     </div>
    </div>
    """
   end
+
+    def handle_event("like", _, socket) do
+      Twitter.Post.inc_likes(socket.assigns.twit)
+      {:noreply, socket}
+    end
+
+    def handle_event("repost", _, socket) do
+      Twitter.Post.inc_reposts(socket.assigns.twit)
+      {:noreply, socket}
+    end
 end
